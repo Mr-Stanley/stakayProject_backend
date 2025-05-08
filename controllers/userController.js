1c6253222090fd07b4703fc4c553942251c435a2
Stanleys world <Stanleyugoo5@gmail.com>
2025-05-08 15:34:04 +0100

    another commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const userController = { register : async (req, res) => {
